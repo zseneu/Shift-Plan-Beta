be1220d0d86aa31934c1c7c25fdabdadd75d12f3
BinaryGeometry <hello@binarygeometry.net>
2014-04-28 23:39:48 +0300

    scss include path refactor

--- js/mw_onestepcheckout/shipping.js
+++ js/mw_onestepcheckout/shipping.js
@@ -1,14 +1,14 @@
 /**
  * 
  */
-$MW_Onestepcheckout(function(){
+jQuery(function(){
 
 	if(zip_load()){	
-				var val_zipship_before=$MW_Onestepcheckout('#shipping\\:postcode').val();
-				$MW_Onestepcheckout('#shipping\\:postcode').blur(function(event){
+				var val_zipship_before=jQuery('#shipping\\:postcode').val();
+				jQuery('#shipping\\:postcode').blur(function(event){
 					val=this.value;
 					if(val!="" && val_zipship_before!=val){					
-						if($MW_Onestepcheckout('#shipping\\:country_id').val())						
+						if(jQuery('#shipping\\:country_id').val())						
 							updateShippingType();
 					}
 					val_zipship_before=val;
@@ -16,11 +16,11 @@ $MW_Onestepcheckout(function(){
 			}
 	
 	if(region_load()){
-		var val_regionship_before=$MW_Onestepcheckout('#shipping\\:region').val();
-		$MW_Onestepcheckout('#shipping\\:region').blur(function(event){
+		var val_regionship_before=jQuery('#shipping\\:region').val();
+		jQuery('#shipping\\:region').blur(function(event){
 			val=this.value;
 			if(val!="" && val_regionship_before!=val){					
-				if($MW_Onestepcheckout('#shipping\\:country_id').val())						
+				if(jQuery('#shipping\\:country_id').val())						
 					updateShippingType();
 			}
 			val_regionship_before=val;
@@ -28,11 +28,11 @@ $MW_Onestepcheckout(function(){
 	}
 	
 	if(city_load()){
-		var val_cityship_before=$MW_Onestepcheckout('#shipping\\:city').val();
-		$MW_Onestepcheckout('#shipping\\:city').blur(function(event){
+		var val_cityship_before=jQuery('#shipping\\:city').val();
+		jQuery('#shipping\\:city').blur(function(event){
 			val=this.value;
 			if(val!="" && val_cityship_before!=val){					
-				if($MW_Onestepcheckout('#shipping\\:country_id').val())						
+				if(jQuery('#shipping\\:country_id').val())						
 					updateShippingType();
 			}
 			val_cityship_before=val;
